56873a8dfce07ccad313215996d4c22ef10e266b
Allison Chen <allison.chen@oracle.com>
2020-10-02 17:45:24 -0500

    removed comments

--- ESP8266_sensor_code/main/wifi_functions.ino
+++ ESP8266_sensor_code/main/wifi_functions.ino
@@ -1,8 +1,10 @@
 void initialize_wifi()
 {
-  //Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+  // Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
   // would try to act as both a client and an access-point and could cause 
   // network-issues with your other WiFi-devices on your WiFi-network.
+
+  // Uncomment the Serial.print statements to debug wifi connection
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 //  Serial.print("connecting");
